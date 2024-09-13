103a8e200372174b8c39584c49ed50c66116e93f
kaschlottag <kaschlottag@gmail.com>
2024-09-13 08:56:04 -0300

    Add files via upload

---
+++ AULA01_JS_KARINA_ANDRE/1-meuPrimeiroPrograma.js
@@ -0,0 +1,20 @@
+//primeira instrução em js
+console.log("\nolá mundo cruel da programação\n");
+
+const aluno1 = "karina";
+const aluno2 = "andré";
+
+const idadeAluno1 = 16;
+const idadeAluno2 = 16;
+
+const somaIdades = idadeAluno1 + idadeAluno2;
+
+const dupla = aluno1 + "e" + aluno2;
+
+console.log("somos a dupla: " + dupla + "\n");
+console.log("A soma da nossas idades é: " + somaIdades + " anos");
+
+document.write("<h1> Somos a dupla: </h1>"+ dupla)
+document.write("A soma de nossas idades é " + somaIdades + " anos");
+
+
